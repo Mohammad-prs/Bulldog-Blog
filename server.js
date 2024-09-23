1d23b8f716bb67d9d71360a71a830f7d0ff75ac4
mohammad-prs <mparsafard@yahoo.com>
2024-09-23 19:45:19 -0400

    Modified code to out put my full name in server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Mohammadreza Parsafard - 121755235") ;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Mohammadreza Parsafard - 121755235');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
