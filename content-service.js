19bb08699a8649744b6a18577c8657b59b589106
mohammad-prs <mparsafard@yahoo.com>
2024-10-14 21:25:58 -0400

    AS2 P2S123

---
+++ content-service.js
@@ -0,0 +1,15 @@
+// Function to initialize
+function initialize() {
+    
+}
+
+// Function to get published articles
+function getPublishedArticles() {
+
+}
+
+// Function to get all categories
+function getCategories() {
+
+}
+
--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3243;
 
-
+// Serve static files from the public folder
 app.use(express.static('public'));
 
+// Redirect root route ('/') to '/about'
 app.get('/', (req, res) => {
     res.redirect('/about');
 });
 
+// Serve about.html when '/about' is accessed
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
+// Serve articles.json for /articles route
+app.get('/articles', (req, res) => {
+    res.sendFile(path.join(__dirname, 'data', 'articles.json'));
+});
+
+// Serve categories.json for /categories route
+app.get('/categories', (req, res) => {
+    res.sendFile(path.join(__dirname, 'data', 'categories.json'));
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Express http server listening on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
